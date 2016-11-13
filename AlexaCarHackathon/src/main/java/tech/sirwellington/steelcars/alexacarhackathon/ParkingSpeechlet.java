08a530db40dae397e66c4611ffde0d62cd2f2cbe
Juan Wellington Moreno <jwellington.moreno@gmail.com>
2016-11-12 23:11:10 -0800

    Adds basic project structure

---
+++ AlexaCarHackathon/src/main/java/tech/sirwellington/steelcars/alexacarhackathon/ParkingSpeechlet.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ 
+package tech.sirwellington.steelcars.alexacarhackathon;
+
+
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tech.aroma.client.Aroma;
+
+/**
+ *
+ * @author SirWellington
+ */
+class ParkingSpeechlet implements Speechlet
+{
+    private final static Logger LOG = LoggerFactory.getLogger(ParkingSpeechlet.class);
+    
+    private final Aroma aroma = Aroma.create("d4592e6b-cb53-4dea-945d-52043abbec84");
+ 
+    @Override
+    public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException
+    {
+        aroma.begin()
+            .titled("Session Started")
+            .text("Request ID: {}, \n SessionID: {}", request.getRequestId(), session.getSessionId())
+            .send();
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(LaunchRequest request, Session session) throws SpeechletException
+    {
+        aroma.begin()
+            .titled("Speechlet Launched")
+            .text("Request ID: {}\nSessionID: {}", request.getRequestId(), session.getSessionId())
+            .send();
+        
+        return createWelcomeMessage();
+    }
+
+    @Override
+    public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException
+    {
+        aroma.begin()
+            .titled("Speechlet Launched")
+            .text("Intent: {}\nSession ID: {}", request.getIntent(), session.getSessionId())
+            .send();
+        return createWelcomeMessage();
+    }
+
+    @Override
+    public void onSessionEnded(SessionEndedRequest request, Session session) throws SpeechletException
+    {
+        aroma.begin()
+            .titled("Session Ended")
+            .text("Session ID: {}", session.getSessionId())
+            .send();
+    }
+    
+     /**
+     * Creates and returns a {@code SpeechletResponse} with a welcome message.
+     *
+     * @return SpeechletResponse spoken and visual response for the given intent
+     */
+    private SpeechletResponse createWelcomeMessage()
+    {
+        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("HelloWorld");
+        card.setContent(speechText);
+
+        // Create the plain text output.
+        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
+        speech.setText(speechText);
+
+        // Create reprompt
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(speech);
+
+        return SpeechletResponse.newAskResponse(speech, reprompt, card);
+    }
+
+}
---
+++ AlexaCarHackathon/src/main/java/tech/sirwellington/steelcars/alexacarhackathon/ParkingSpeechletHandler.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ 
+package tech.sirwellington.steelcars.alexacarhackathon;
+
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sir.wellington.alchemy.collections.sets.Sets;
+
+/**
+ *
+ * @author SirWellington
+ */
+class ParkingSpeechletHandler extends SpeechletRequestStreamHandler
+{
+    private final static Logger LOG = LoggerFactory.getLogger(ParkingSpeechletHandler.class);
+
+    private static final Set<String> APPLICATION_IDS = Sets.createFrom("amzn1.ask.skill.a01de60e-d3d4-4983-85d7-00a3ce26144c");
+    
+    public ParkingSpeechletHandler()
+    {
+        super(new ParkingSpeechlet(), APPLICATION_IDS);
+    }
+
+}
